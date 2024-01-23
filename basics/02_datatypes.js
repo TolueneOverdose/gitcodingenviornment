6c783f403dde8f059aa6b998559d1a818cf97ee6
tapan <tapankapre67@gmail.com>
2024-01-23 22:35:53 +0530

    lecture 2 done

--- basics/02_datatypes.js
+++ basics/02_datatypes.js
@@ -1,1 +1,13 @@
-
+//numbers
+let num=5
+let num2=3.33
+let num3=10/3
+//string
+let st="hello world"
+//boolean
+let f=false
+//object
+let obj=NaN
+//null-> standalone value (a object)
+//undefined=> not defined yet (a type of datatype)
+//symbol => unique (used to differentiate components)
